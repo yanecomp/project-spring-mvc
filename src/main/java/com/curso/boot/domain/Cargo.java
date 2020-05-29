0b6c4df96e99f13e6a8b4e7bd49da2594d6fff35
Yan Cavalcante <yan.ecomp@gmail.com>
2020-05-29 16:14:25 -0300

    Criação das entidades, controllers, dao e services.

--- src/main/java/com/curso/boot/domain/Cargo.java
+++ src/main/java/com/curso/boot/domain/Cargo.java
@@ -3,12 +3,7 @@ package com.curso.boot.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @SuppressWarnings("serial")
 @Entity
