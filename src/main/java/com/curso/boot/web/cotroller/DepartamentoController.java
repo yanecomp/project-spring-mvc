0b67290ecf935bb7c0734eaf02b1d7f858bfacc1
Yan Cavalcante <yan.ecomp@gmail.com>
2020-06-02 15:27:22 -0300

    Desenvolvimento de funcionalidades para cadastro e exibição de Departamentos.

--- src/main/java/com/curso/boot/web/cotroller/DepartamentoController.java
+++ src/main/java/com/curso/boot/web/cotroller/DepartamentoController.java
@@ -1,21 +1,38 @@
 package com.curso.boot.web.cotroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.curso.boot.domain.Departamento;
+import com.curso.boot.service.DepartamentoService;
+
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
 	
+	@Autowired
+	private DepartamentoService service;
+	
 	@GetMapping("/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("departamentos", service.buscarTodos());
 		return "/departamento/lista";
 	}
 	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Departamento departamento) {
 		return "/departamento/cadastro";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Departamento departamento) {
+		service.salvar(departamento);
+		return "redirect:/departamentos/cadastrar";
 	}
 
 }
